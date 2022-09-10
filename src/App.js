6142d8f351c3be3697e47f220986039c4c630a5f
MMLaw95 <103419849+MMLaw95@users.noreply.github.com>
2022-09-10 06:21:18 -0500

    Added search engine to dictionary component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
-
+import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          <small>Open-source by M.M. Lawry</small>
+        </footer>
+      </div>
     </div>
   );
 }
