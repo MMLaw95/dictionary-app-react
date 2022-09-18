0cf2f36aab4c22860c95f43eec018fb6e355f36b
MMLaw95 <103419849+MMLaw95@users.noreply.github.com>
2022-09-18 12:16:19 -0500

    Add photo section

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,23 +2,38 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
     event.preventDefault();
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000019aafb4bf44df4331bb41acdf2af6227d";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    axios
+      .get(pexelsApiUrl, {
+        headers: { Authorization: `Bearer ${pexelsApiKey}` },
+      })
+      .then(handlePexelsResponse);
   }
 
-  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-  axios.get(apiUrl).then(handleResponse);
-
-  function handleKeywordChange(event) {
+  function keywordInput(event) {
     setKeyword(event.target.value);
   }
 
@@ -28,7 +43,7 @@ export default function Dictionary() {
         <form onSubmit={search} className="inputForm text-center">
           <input
             type="search"
-            onChange={handleKeywordChange}
+            onChange={keywordInput}
             autoComplete="off"
             placeholder="Look it Up..."
           />
@@ -38,6 +53,7 @@ export default function Dictionary() {
         </div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
