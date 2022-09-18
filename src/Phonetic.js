e34cb3e741e81e8af69901f0d925e901d5df446d
MMLaw95 <103419849+MMLaw95@users.noreply.github.com>
2022-09-18 15:01:04 -0500

    Fixing href

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -8,7 +8,7 @@ export default function Phonetic(props) {
   }
   return (
     <div className="Phonetic red text-center">
-      <a href="#" onClick={playSound}>
+      <a href="/#" onClick={playSound}>
         <i class="fa-solid fa-headphones-simple fa-2xl"></i>
       </a>
       <span className="phonetic-text">{props.phonetic.text}</span>
