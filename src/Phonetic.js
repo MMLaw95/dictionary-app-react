39e0351f6b70c054b1d9e30152845dbaf04069cf
MMLaw95 <103419849+MMLaw95@users.noreply.github.com>
2022-09-18 22:36:15 -0500

    Update Phonetic.js

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,18 +2,22 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  function playSound() {
-    let audio = new Audio(props.phonetic.audio);
-    audio.play();
+  if (props.phonetic) {
+    function playSound() {
+      let audio = new Audio(props.phonetic.audio);
+      audio.play();
+    }
+    return (
+      <div className="Phonetic red text-center">
+        <a href="/#" onClick={playSound}>
+          <i class="fa-solid fa-headphones-simple fa-2xl"></i>
+        </a>
+        <span className="phonetic-text">{props.phonetic.text}</span>
+      </div>
+    );
+  } else {
+    return null;
   }
-  return (
-    <div className="Phonetic red text-center">
-      <a href="/#" onClick={playSound}>
-        <i class="fa-solid fa-headphones-simple fa-2xl"></i>
-      </a>
-      <span className="phonetic-text">{props.phonetic.text}</span>
-    </div>
-  );
 }
 
 //   return (
